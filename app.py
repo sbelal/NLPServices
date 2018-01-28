b928aa747ffc403bd1a1a56e3fe2bc635de08235
Salamander <sbelal@gmail.com>
2018-01-28 13:39:21 -0800

    get with params

--- app.py
+++ app.py
@@ -3,26 +3,77 @@ Flask service test
 """
 
 import tensorflow as tf
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
-@app.route("/")
 
+#region error handlers
 
-def home0():
+@app.errorhandler(404)
+def not_found(error):
+	'''
+	Error 404 handling
+	'''
+	return make_response(jsonify({'error': str(error)}), 404)
+
+@app.errorhandler(500)
+def internal_error(error):
+	'''
+	Error 500 handling
+	'''
+	return make_response(jsonify({'error': 'Internal server error. Details: '+str(error)}), 500)
+
+#endregion
+
+#region service functions
+
+@app.route('/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
 	"""
 	qwqeqw eqweq we qwe qwe
 	"""
-	return str(42)
+	task = [task for task in tasks if task['id'] == task_id]
+	if not task:
+		abort(404)
+	return jsonify({'task': task[0]})
 
-
-def home1():
+@app.route('/api/v1.0/tensortest/<text>', methods=['GET'])
+def home1(text):
 	"""
 	qwqeqw eqweq we qwe qwe
 	"""
-	return sess.run(hello)
+	tensorOutput = str(sess.run(hello))
+	finalOutput = tensorOutput + text
+	return finalOutput
 
 
+@app.route('/api/v1.0/myerror', methods=['GET'])
+def myerror():
+	"""
+	qwqeqw eqweq we qwe qwe
+	"""
+
+	tensorOutput = sess.run(hello)
+	finalOutput = tensorOutput + "a"
+	return finalOutput
+#endregion
+
+#region fake data
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+#endregion
 
 
 hello = tf.constant('Hello, TensorFlow!')
@@ -31,4 +82,4 @@ sess = tf.Session()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
