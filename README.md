cdb32c66eb109669968b9a7dfe6410e655e91127
Salamander <sbelal@gmail.com>
2018-01-29 03:05:52 -0800

    Docker stuff

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NLPServices
 NLP Services.  Each service can be run as a docker container.
+
+
+
+docker build -t flask-sample-one:latest .
+
+docker run -it -p 5000:5000 flask-sample-one:latest 
+
+
+http://guido-barbaglia.blog/posts/use_docker_to_run_flask_based_rest_services.html
+
+
+
+docker stop $(docker ps -aq);docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ Flask service test
 """
 
 import tensorflow as tf
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, request, make_response, url_for
 
 app = Flask(__name__)
 
@@ -11,52 +11,126 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-	'''
-	Error 404 handling
-	'''
-	return make_response(jsonify({'error': str(error)}), 404)
+    '''
+    Error 404 handling
+    '''
+    return make_response(jsonify({'error': str(error)}), 404)
 
 @app.errorhandler(500)
 def internal_error(error):
-	'''
-	Error 500 handling
-	'''
-	return make_response(jsonify({'error': 'Internal server error. Details: '+str(error)}), 500)
+    '''
+    Error 500 handling
+    '''
+    return make_response(jsonify({'error': 'Internal server error. Details: '+str(error)}), 500)
 
 #endregion
 
 #region service functions
 
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    """
+    qwqeqw eqweq we qwe qwe
+    """
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
 @app.route('/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_tasks(task_id):
-	"""
-	qwqeqw eqweq we qwe qwe
-	"""
-	task = [task for task in tasks if task['id'] == task_id]
-	if not task:
-		abort(404)
-	return jsonify({'task': task[0]})
+def get_single_task(task_id):
+    """
+    qwqeqw eqweq we qwe qwe
+    """
+    task = [task for task in tasks if task['id'] == task_id]
+    if not task:
+        abort(404)
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/api/v1.0/tensortest/<text>', methods=['GET'])
 def home1(text):
-	"""
-	qwqeqw eqweq we qwe qwe
-	"""
-	tensorOutput = str(sess.run(hello))
-	finalOutput = tensorOutput + text
-	return finalOutput
+    """
+    qwqeqw eqweq we qwe qwe
+    """
+    tensorOutput = str(sess.run(hello))
+    finalOutput = tensorOutput + text
+    return finalOutput
 
 
 @app.route('/api/v1.0/myerror', methods=['GET'])
 def myerror():
-	"""
-	qwqeqw eqweq we qwe qwe
-	"""
+    """
+    qwqeqw eqweq we qwe qwe
+    """
 
-	tensorOutput = sess.run(hello)
-	finalOutput = tensorOutput + "a"
-	return finalOutput
+    tensorOutput = sess.run(hello)
+    finalOutput = tensorOutput + "a"
+    return finalOutput
+
+
+@app.route('/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    '''
+    Some text
+    '''
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+
+def update_task(task_id):
+    '''
+    blah blah
+    '''
+    task = [task for task in tasks if task['id'] == task_id]
+    if not task:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and not isinstance(request.json['title'], str):
+        abort(400)
+    if 'description' in request.json and not isinstance(request.json['description'], str):
+        abort(400)
+    if 'done' in request.json and not isinstance(request.json['done'], bool):
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    '''
+    blah blah
+    '''
+    task = [task for task in tasks if task['id'] == task_id]
+    if not task:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
 #endregion
+
+
+def make_public_task(task):
+    '''
+    Helper
+    '''
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_single_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 
 #region fake data
 tasks = [
@@ -82,4 +156,4 @@ sess = tf.Session()
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
